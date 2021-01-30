3bdfcd243c1ea99568d156a41837f7255f19df1a
thing44 <64006897+thing44@users.noreply.github.com>
2021-01-30 17:47:11 +0900

    test

--- DogCat/app/src/main/java/com/example/midtwenties/InfoActivity.kt
+++ DogCat/app/src/main/java/com/example/midtwenties/InfoActivity.kt
@@ -1,5 +1,6 @@
 package com.example.midtwenties
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageSwitcher
@@ -10,6 +11,8 @@ class InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        var button=0;
 
         femaleButton.setOnClickListener{
             Toast.makeText(applicationContext,"암컷을 입양합니다.",Toast.LENGTH_SHORT).show()
@@ -22,16 +25,27 @@ class InfoActivity : AppCompatActivity() {
         babyButton.setOnClickListener{
             Toast.makeText(applicationContext,"1개월-24개월",Toast.LENGTH_SHORT).show()
             petImage.setImageResource(R.drawable.baby_dog)
+            button=1
         }
-
         adultButton.setOnClickListener{
             Toast.makeText(applicationContext,"25개월-84개월",Toast.LENGTH_SHORT).show()
             petImage.setImageResource(R.drawable.adult_dog)
+            button=2
         }
-
         oldButton.setOnClickListener{
             Toast.makeText(applicationContext,"85개월-",Toast.LENGTH_SHORT).show()
             petImage.setImageResource(R.drawable.old_dog)
+            button=3
+        }
+
+        saveInfo.setOnClickListener {
+            if(button==0){
+                Toast.makeText(applicationContext,"선택해주세요",Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(applicationContext, "저장되었습니다", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, termsOfService::class.java))
+            }
         }
     }
 }
--- DogCat/app/src/main/java/com/example/midtwenties/SplashActivity.kt
+++ DogCat/app/src/main/java/com/example/midtwenties/SplashActivity.kt
@@ -1,21 +1,42 @@
 package com.example.midtwenties
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import android.text.TextUtils
 
 class SplashActivity : AppCompatActivity() {
+
+    var first=false;
+
+    val filename="init.txt"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+        if(loadFromInnerStorage(filename).equals("true")){
+            startActivity(Intent(this, RealMainActivity::class.java))
+        }
+        else{
+            startActivity(Intent(this, init_pet::class.java))
+        }
 
+        Handler(Looper.getMainLooper()).postDelayed({
             overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
             finish()
         }, 2500)
+
+
     }
+
+    fun loadFromInnerStorage(filename: String):String{
+        val fileInputStream=openFileInput(filename)
+        return fileInputStream.reader().readText()
+    }
+
 }
