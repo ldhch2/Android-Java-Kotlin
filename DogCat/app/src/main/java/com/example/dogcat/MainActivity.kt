13f101d095525cc863317a1ed2ca89b3dcddeae4
MYMY-young <64087601+MYMY-young@users.noreply.github.com>
2021-01-30 15:48:44 +0900

    .
    .

--- DogCat/app/src/main/java/com/example/dogcat/MainActivity.kt
+++ DogCat/app/src/main/java/com/example/dogcat/MainActivity.kt
@@ -1,11 +1,55 @@
-package com.example.dogcat
+package org.techtown.hh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.content.Intent
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var isOpen = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fabOpen = AnimationUtils.loadAnimation(this,R.anim.fab_open)
+        val fabClose = AnimationUtils.loadAnimation(this,R.anim.fab_close)
+
+        fabMain.setOnClickListener{
+            if(isOpen){
+                fab_sub1.startAnimation(fabClose)
+                fab_sub2.startAnimation(fabClose)
+                fab_sub3.startAnimation(fabClose)
+                fab_sub4.startAnimation(fabClose)
+
+
+                isOpen=false
+            }
+            else{
+                fab_sub1.startAnimation(fabOpen)
+                fab_sub2.startAnimation(fabOpen)
+                fab_sub3.startAnimation(fabOpen)
+                fab_sub4.startAnimation(fabOpen)
+
+
+                fab_sub1.isClickable
+                fab_sub2.isClickable
+                fab_sub3.isClickable
+                fab_sub4.isClickable
+
+                isOpen = true
+            }
+
+            fab_sub1.setOnClickListener{
+                Toast.makeText(this,"click",Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+
     }
 }
