ba1cd8153316a0e48bc0b5f44dfb5b3f58574521
younseo-leee <77726052+younseo-leee@users.noreply.github.com>
2021-01-30 15:46:36 +0900

    윤서
    ㅎㅎ

---
+++ DogCat/app/src/main/java/com/example/dogcat/termsOfService.kt
@@ -0,0 +1,11 @@
+package com.akj.midtwenties
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class termsOfService : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_of_service)
+    }
+}
