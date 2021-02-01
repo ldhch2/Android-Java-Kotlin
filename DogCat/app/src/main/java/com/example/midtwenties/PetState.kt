fd3fb96d924675ffb84c34ec0350a57c8d3a4b0e
주어진사랑 <63996052+ozzing@users.noreply.github.com>
2021-02-02 02:29:58 +0900

    0202
    pop-up 전까지

---
+++ DogCat/app/src/main/java/com/example/midtwenties/PetState.kt
@@ -0,0 +1,12 @@
+package com.example.midtwenties
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PetState : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pet_state)
+
+    }
+}
