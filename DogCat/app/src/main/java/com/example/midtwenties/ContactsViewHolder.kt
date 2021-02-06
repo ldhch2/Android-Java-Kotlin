8fd7f0e79c9e4525f182c51f3438175f1cc9ec33
younseo-leee <77726052+younseo-leee@users.noreply.github.com>
2021-02-06 17:19:47 +0900

    d

---
+++ DogCat/app/src/main/java/com/example/midtwenties/ContactsViewHolder.kt
@@ -0,0 +1,12 @@
+package com.example.midtwenties
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactsViewHolder(v: View) : RecyclerView.ViewHolder(v)
+{
+    var view : View = v
+    fun bind(item: Contacts) {
+
+    }
+}
