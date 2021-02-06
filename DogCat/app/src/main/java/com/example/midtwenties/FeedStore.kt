5c5a89e9d79f65623042f123774307ca9e2e715b
younseo-leee <77726052+younseo-leee@users.noreply.github.com>
2021-02-06 16:40:55 +0900

    11
    1

--- DogCat/app/src/main/java/com/example/midtwenties/FeedStore.kt
+++ DogCat/app/src/main/java/com/example/midtwenties/FeedStore.kt
@@ -1,11 +1,17 @@
 package com.example.midtwenties
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_my_store.*
 
 class FeedStore : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed_store)
+
+        back.setOnClickListener{
+            startActivity(Intent(this,MyStore::class.java))
+        }
     }
 }
