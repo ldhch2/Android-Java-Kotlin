d7736630c449a4c5238bf19d75c64d00abd33089
주어진사랑 <63996052+ozzing@users.noreply.github.com>
2021-02-18 23:49:18 +0900

    이것저것

--- DogCat/app/src/main/java/com/midtwenties/dogcat/StoreItem.kt
+++ DogCat/app/src/main/java/com/midtwenties/dogcat/StoreItem.kt
@@ -10,7 +10,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_store_item.*
 
 class StoreItem : AppCompatActivity() {
-
     var toyList = arrayListOf<StoreContacts>(
             StoreContacts("", "공", 500, "구매", "빨강색", "파란색", ""),
             StoreContacts("", "삑삑이", 500, "구매", "빨강색", "파란색", "초록색"),
@@ -33,8 +32,10 @@ class StoreItem : AppCompatActivity() {
             StoreContacts("", "물물", 500, "구매", "빨강색", "파란색", "초록색")
     )
     var feedList = arrayListOf<StoreContacts>(
-            StoreContacts("", "강아지 사료", 10000, "구매", "1kg", "3kg", "5kg"),
-            StoreContacts("", "고양이 사료", 10000, "구매", "1kg", "3kg", "5kg"),
+            StoreContacts("dogyum", "강아지 사료", 10000, "구매", "1kg", "3kg", "5kg"),
+            StoreContacts("catyum", "고양이 사료", 10000, "구매", "1kg", "3kg", "5kg"),
+            StoreContacts("dogyumpre", "[Premium] 강아지 사료", 15000, "구매", "1kg", "3kg", "5kg"),
+            StoreContacts("catyumpre", "[Premium] 고양이 사료", 15000, "구매", "1kg", "3kg", "5kg"),
             StoreContacts("", "뼈다귀(강아지용)", 500, "구매", "", "", ""),
             StoreContacts("", "참치(고양이용)", 500, "구매", "", "", "")
     )
@@ -44,11 +45,11 @@ class StoreItem : AppCompatActivity() {
             StoreContacts("", "옷옷", 500, "구매", "빨강색", "파란색", "초록색")
     )
     fun reset() {
-        goToyStore.setBackgroundColor(Color.parseColor("#00FF0000"))
-        goHouseStore.setBackgroundColor(Color.parseColor("#00FF0000"))
-        goBowlStore.setBackgroundColor(Color.parseColor("#00FF0000"))
-        goFeedStore.setBackgroundColor(Color.parseColor("#00FF0000"))
-        goClothesStore.setBackgroundColor(Color.parseColor("#00FF0000"))
+        goToyStore.isEnabled=true
+        goHouseStore.isEnabled=true
+        goBowlStore.isEnabled=true
+        goFeedStore.isEnabled=true
+        goClothesStore.isEnabled=true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,31 +66,31 @@ class StoreItem : AppCompatActivity() {
             reset()
             val adapter = ContactsListAdapter(this, toyList)
             StoreRecyclerview.adapter = adapter
-            goToyStore.setBackgroundColor(Color.parseColor("#22741C"))
+            goToyStore.isEnabled=false
         }
         goHouseStore.setOnClickListener{
             reset()
             val adapter = ContactsListAdapter(this, houseList)
             StoreRecyclerview.adapter = adapter
-            goHouseStore.setBackgroundColor(Color.parseColor("#22741C"))
+            goHouseStore.isEnabled=false
         }
         goBowlStore.setOnClickListener{
             reset()
             val adapter = ContactsListAdapter(this, bowlList)
             StoreRecyclerview.adapter = adapter
-            goBowlStore.setBackgroundColor(Color.parseColor("#22741C"))
+            goBowlStore.isEnabled=false
         }
         goFeedStore.setOnClickListener{
             reset()
             val adapter = ContactsListAdapter(this, feedList)
             StoreRecyclerview.adapter = adapter
-            goFeedStore.setBackgroundColor(Color.parseColor("#22741C"))
+            goFeedStore.isEnabled=false
         }
-        goClothesStore.setOnClickListener{
+        goClothesStore.setOnClickListener {
             reset()
             val adapter = ContactsListAdapter(this, clothesList)
             StoreRecyclerview.adapter = adapter
-            goClothesStore.setBackgroundColor(Color.parseColor("#22741C"))
+            goClothesStore.isEnabled =false
         }
 
         val lay = LinearLayoutManager(this)
