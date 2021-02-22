079a98fb61a0783be3b41aa9e2501ad21ac99e1d
주어진사랑 <63996052+ozzing@users.noreply.github.com>
2021-02-23 02:04:20 +0900

    idcard 생성 액티비티들
    펫 정보 넘기기

--- DogCat/app/src/main/java/com/midtwenties/dogcat/PetIDActivity.kt
+++ DogCat/app/src/main/java/com/midtwenties/dogcat/PetIDActivity.kt
@@ -7,36 +7,66 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.ViewCompat.setBackground
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_pet_id.*
 import kotlinx.android.synthetic.main.activity_pet_id.petImage
-import kotlinx.android.synthetic.main.activity_register.*
-import kotlinx.android.synthetic.main.activity_terms_of_service.view.*
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.OutputStream
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.Month
+import java.time.format.DateTimeFormatter
+import java.util.*
+import java.util.Calendar.MONTH
 
 class PetIDActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pet_id)
 
         val next=Intent(this,TermsOfService::class.java)
-        var kind: Int = intent.getIntExtra("kind",0)
-        if(kind==1) Glide.with(this).load(R.drawable.basicdog).into(petImage)
-        else if(kind==2) Glide.with(this).load(R.drawable.basiccat).into(petImage)
+
 
         val get=intent.getStringExtra("info").toString()
         next.putExtra("info",get)
+        val arr=get.split(" ")
+        val name=arr[0]
+        val kind=arr[1].toInt()
+        val month=arr[2].toInt()
+        val gender=arr[3].toInt()
 
+        val today= SimpleDateFormat("yyyy-MM-dd", Locale.KOREA).format(Date())
+        val now=LocalDate.now()
+        var birth=now
+        when (month) {
+            1 ->  birth=now.minusMonths(1)
+            25 -> birth=now.minusMonths(25)
+            85 -> birth=now.minusMonths(85)
+        }
+        val birthday: String = birth.format(DateTimeFormatter.ofPattern("yyMMdd"))
+
+
+        if(kind==1) Glide.with(this).load(R.drawable.basicdog).into(petImage)
+        else if(kind==2) Glide.with(this).load(R.drawable.basiccat).into(petImage)
+        petidName.setText(name)
+        if(gender==1) petidGender.setText("女")
+        else if (gender==2) petidGender.setText("男")
+        petidBirthday.setText(birthday)
 
         val uri: Uri? =intent.getParcelableExtra("signature")
         signature.setImageURI(uri)
+        petidDay.setText(today)
 
         save.setOnClickListener{
             val cardBitmap =captureView(findViewById(R.id.idcardView), idcardView.height, idcardView.width)
